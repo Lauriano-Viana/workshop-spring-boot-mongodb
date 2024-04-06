82feaf92aab5b4cd348dacf5e7412fc732a762a7
Lariano <laurianoviana12@gmail.com>
2024-04-06 11:48:13 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/lauriscode/workshopmongo/resources/UserResource.java
+++ src/main/java/com/lauriscode/workshopmongo/resources/UserResource.java
@@ -1,28 +1,25 @@
 package com.lauriscode.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lauriscode.workshopmongo.domain.User;
+import com.lauriscode.workshopmongo.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
-	
-@GetMapping
-public ResponseEntity<List<User>> findAll(){
-   var maria = new User("1","Maria silva","maria@gmail.com");
-   var alex = new User("2","Alex silva","alex@gmail.com");
-  List<User> list = new ArrayList<>();
-  list.addAll(Arrays.asList(maria,alex));
-  return ResponseEntity.ok(list);
-  }
+	@Autowired
+	private UserService userservice;
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userservice.findAll();
+		return ResponseEntity.ok().body(list);
+	}
 }
-
-
