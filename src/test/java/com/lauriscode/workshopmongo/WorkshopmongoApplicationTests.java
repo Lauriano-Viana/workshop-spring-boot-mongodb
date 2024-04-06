db9dc53b826b5513f46039f205ab22b9b142aa79
Lariano <laurianoviana12@gmail.com>
2024-04-06 10:17:06 -0300

    project created

---
+++ src/test/java/com/lauriscode/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lauriscode.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
